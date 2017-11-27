9d72da0c12d2934753c4e91bed9ae7afffc48609
zouh9907 <zouh@rose-hulman.edu>
2017-11-27 09:05:28 -0500

    finish m3

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -63,6 +63,8 @@ dave = rg.SimpleTurtle()
 dave.forward(100)
 dave.left(90)
 dave.forward(200)
+dave.left(45)
+dave.forward(80)
 
 # ----------------------------------------------------------------------
 # Construct a new turtle and ask it to do things.
@@ -77,7 +79,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +93,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +111,15 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+apple = rg.SimpleTurtle()
+apple.speed = 3
+apple.right(90)
+apple.backward(100)
+apple.right(90)
+apple.forward(100)
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
