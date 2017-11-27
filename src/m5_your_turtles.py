9443530559e41076f4ea26c561db11635db8e002
zouh9907 <zouh@rose-hulman.edu>
2017-11-27 09:31:41 -0500

    finish m5

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -10,7 +10,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
